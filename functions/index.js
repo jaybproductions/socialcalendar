d318655cc89f2a2ffad215e4578d729bf3bcf6a1
Chris - BTWG <chris@btwebgroup.com>
2020-12-22 11:09:10 -0500

    Testing

--- functions/index.js
+++ functions/index.js
@@ -22,7 +22,7 @@ const port = 85;
 });*/
 
 const s3 = new AWS.S3({
-  accessKeyId: process.env.AWS_ACCESS_KEY,
+  accessKeyId: process.env.AWS_ACCESS_ID,
   secretAccessKey: process.env.AWS_SECRET_KEY,
   region: 'us-east-2'
 });
