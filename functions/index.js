1ac6aaaa768eeeb45e41d2a98a1611debd0dddef
Chris - BTWG <chris@btwebgroup.com>
2020-12-22 11:01:21 -0500

    Testing

--- functions/index.js
+++ functions/index.js
@@ -24,6 +24,7 @@ const port = 85;
 const s3 = new AWS.S3({
   accessKeyId: process.env.AWS_ACCESS_KEY,
   secretAccessKey: process.env.AWS_SECRET_KEY,
+  region: 'us-east-2'
 });
 
 const uploadS3 = multer({
